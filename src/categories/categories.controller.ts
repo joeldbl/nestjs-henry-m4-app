5e7e7d5e0c51bd685a18b2b9b1ee47af972e63b9
Joel Blanco <joeld.blanco@gmail.com>
2024-04-22 15:13:07 -0500

    Add @ApiTags('categories') to CategoriesController

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('categories')
 @Controller('categories')
